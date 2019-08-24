b8db59dbbd7f5cacfa6dbe41f990eaa686abd081
Hugo Alejandro Ramos Garcia <hramos0@ucol.mx>
2019-08-23 23:08:53 -0500

    Login

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "juancho")
+            {
+                if (textBox2.Text == "123tamarindo")
+                    MessageBox.Show("Accediste correctamente");
+                else
+                    MessageBox.Show("Tu contraseñaes incorrecta");
+            }
+            else
+                MessageBox.Show("Tu usuario es incorrecto");
+        }
+    }
+}
